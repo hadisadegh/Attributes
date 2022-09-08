f73d2d3081491bd6139a8b8c486345de2ad07e85
noviraweb <54057074+hadisadegh@users.noreply.github.com>
2022-09-08 11:32:18 +0430

    Add files via upload

---
+++ ExceptionHandlerAsync.cs
@@ -0,0 +1,39 @@
+﻿
+using DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace Web.Attributes
+{
+    public class ExceptionHandlerAsync : ExceptionFilterAttribute
+    {
+        public string DefaultErrorMessage { get; set; }
+        //=============================================================================
+        public override async Task OnExceptionAsync(ExceptionContext context)
+        {
+            await Task.Run(() =>
+            {
+
+                if (context.Exception.GetType().Name == "CustomException")
+                {
+                    context.Result = new BadRequestObjectResult(new SysResult<string>() { IsSuccess = false, Message = context.Exception.Message });
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(DefaultErrorMessage))
+                    {
+                        context.Result = new BadRequestObjectResult(new SysResult<string>() { IsSuccess = false, Message = DefaultErrorMessage });
+                    }
+                    else
+                    {
+                        context.Result = new BadRequestObjectResult(new SysResult<string>() { IsSuccess = false, Message = context.Exception.Message });
+                    }
+                }
+                return base.OnExceptionAsync(context);
+            });
+        }
+        //=============================================================================
+
+    }
+}
---
+++ ModelValidatorAsync.cs
@@ -0,0 +1,30 @@
+﻿using DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Attributes
+{
+    public class ModelValidatorAsync : ActionFilterAttribute
+    {
+        //===========================================================================
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            await Task.Run(() =>
+           {
+               if (!context.ModelState.IsValid)
+               {
+                   var errorMessages = string.Empty;
+                   var errors = context.ModelState.Where(c => c.Value.ValidationState == ModelValidationState.Invalid).ToList();
+                   foreach (var error in errors)
+                       errorMessages += error.Value.Errors[0].ErrorMessage + "\n";
+                   context.Result = new BadRequestObjectResult(new SysResult<string>() { IsSuccess = false, Message = errorMessages, Value = null });
+               }
+               return base.OnActionExecutionAsync(context, next);
+           });
+        }
+        //=============================================================================
+    }
+}
